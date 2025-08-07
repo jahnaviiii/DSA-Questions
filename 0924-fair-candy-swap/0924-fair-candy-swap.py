23e04ecd60483856af8a8724ded0d3d15eb97f95
jahnaviiii <jahnavinagesh92@gmail.com>
2025-08-07 16:12:19 +0530

    Time: 7 ms (73.82%), Space: 20 MB (36.83%) - LeetHub

--- 0924-fair-candy-swap/0924-fair-candy-swap.py
+++ 0924-fair-candy-swap/0924-fair-candy-swap.py
@@ -4,9 +4,15 @@ class Solution:
         sumB = sum(bobSizes)
 
         delta = (sumB - sumA) // 2
+        # if x is the candy to be removed from alice and y is the candy to be removed from bob
+        # we just have to make sure that sumAlice - x + y == sumBob - y + x
+        # after solving this, we get y = (sumBob - sumAlice)// 2 + x
+        # we can consider delta = (sumBob - sumAlice) // 2
 
         bobSet = set(bobSizes)
 
+        # for every candy in alice, if delta + candy exists in bob, then that can be exchanged
+        # to maintain equal sum
         for x in aliceSizes:
             if delta + x in bobSet:
                 return [x, delta + x] 
